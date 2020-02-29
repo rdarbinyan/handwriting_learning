08746024bad4d5547867bbd7412ac004c03ec28a
rafayel <darbinyanraf@gmail.com>
2020-02-29 10:40:52 +0400

    exported model

--- convertToJson.sh
+++ convertToJson.sh
@@ -1,2 +1,2 @@
-tensorflowjs_converter --input_format keras tmp/decoder.h5 output
-tensorflowjs_converter --input_format keras tmp/encoder.h5 output
+tensorflowjs_converter --input_format keras tmp/decoder.h5 output/decoder.json
+tensorflowjs_converter --input_format keras tmp/encoder.h5 output/encoder.json
